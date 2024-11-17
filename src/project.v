cd988e85cb95aa9366a0c25f38e82788ae014e9a
Virantha Ekanayake <virantha@gmail.com>
2024-11-16 23:15:10 -0500

    first commit

--- src/project.v
+++ src/project.v
@@ -17,11 +17,26 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  reg [7:0] cnt;
+
+  always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        cnt <= 0;
+    end
+    else begin 
+        cnt <= cnt+1;
+    end
+  end
+
+  assign uo_out = cnt;
+
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = ena;
+  //wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
